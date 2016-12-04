3fea7c7937023ec279af2f2caa45711aef890d46
John DiBaggio <john.dibaggio@vml.com>
2016-12-03 23:43:04 -0700

    Working on multithreaded hamming distance calculator

--- rosalind/src/ex_6__counting_point_mutations.py
+++ rosalind/src/ex_6__counting_point_mutations.py
@@ -6,6 +6,7 @@ Created by John DiBaggio on 2016-12-03
 __author__ = 'johndibaggio'
 
 import sys
+import time
 
 argv = list(sys.argv)
 output_file = open(argv[2], 'w+')
@@ -23,7 +24,7 @@ if len(strands) >= 2:
 
 def calc_hamming_distance(s1, s2):
     if s1 is None or s2 is None or len(s1) != len(s2):
-        raise ValueError("Both strands must not be empty and must be of equal length")
+        raise ValueError("Both strands must not be empty and must be of equal length.")
     h_dist = 0
     for index, n_base in enumerate(s1):
         if n_base != s2[index]:
@@ -31,7 +32,13 @@ def calc_hamming_distance(s1, s2):
     return h_dist
 
 try:
+    time_start = time.time()
+
     hamming_distance = calc_hamming_distance(strand_1, strand_2)
+
+    time_end = time.time()
+    time_elapsed = time_end - time_start
+    print "Hamming distance is {}. Calculated in {} seconds".format(str(hamming_distance), time_elapsed)
     output_file.write(str(hamming_distance))
 except ValueError:
     print "ValueError thrown while calculating Hamming distance for strands:\n{}\n{}".format(strand_1, strand_2)
